7b9582ab8bea2c9fa139c89e8f0d10f68f8286d7
Kevin Stephen Biswas <kevinstephenbiswas@gmail.com>
2023-02-26 20:41:34 +0900

    Added FinancialData model

--- financial/models.py
+++ financial/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class FinancialData(models.Model):
+    symbol = models.CharField(max_length=20)
+    date = models.DateField()
+    open_price = models.DecimalField(max_digits=20, decimal_places=2)
+    close_price = models.DecimalField(max_digits=20, decimal_places=2)
+    volume = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        db_table = "financial_data"
+    
